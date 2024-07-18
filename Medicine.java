6d4551de892465fbb50e2459d66861db395c1408
unknown <bhagyahiremath.xworkz@gmail.com>
2024-07-18 19:04:15 +0530

    message

---
+++ Medicine.java
@@ -0,0 +1,11 @@
+class Medicine{
+public static void medicineInfos(String name,String manfdate,double price,int quantity)
+{
+System.out.println("running medicineInfos in Oval");
+System.out.println("Name:"+name);
+System.out.println("Manfdate:"+manfdate);
+System.out.println("Price:"+price);
+System.out.println("Quantity:"+quantity);
+
+}
+}
